6c068e9ff3276d593501c4fc4fcc3846d5ebf818
Jun Ryoung Ju <junryoungju@gmail.com>
2026-02-20 17:36:33 +0900

    Add PrintAssumptions

---
+++ PrintAssumptions.v
@@ -0,0 +1,44 @@
+Load "개똥철학".
+
+(* === Core === *)
+Print Assumptions dichotomy.
+Print Assumptions distinguish_at.
+Print Assumptions native_identity_is_absolute.
+
+(* === Reify tower === *)
+Print Assumptions reify_dim_neq.
+Print Assumptions no_self_containment.
+Print Assumptions level_separation.
+Print Assumptions dim_identity_decidable_at.
+
+(* === Uniformity === *)
+Print Assumptions entity_has_domain.
+Print Assumptions entity_has_operator.
+Print Assumptions dimension_has_domain.
+Print Assumptions dimension_has_operator.
+Print Assumptions every_level_has_domain.
+Print Assumptions every_level_has_operator.
+
+(* === Projection === *)
+Print Assumptions project_irrel.
+Print Assumptions projection_reflects_diff.
+Print Assumptions coarsening_widens.
+
+(* === Equivalence === *)
+Print Assumptions dim_equiv_refl.
+Print Assumptions dim_equiv_sym.
+Print Assumptions dim_equiv_trans.
+
+(* === Comparability === *)
+Print Assumptions comp_at_refl.
+Print Assumptions comp_at_sym.
+Print Assumptions comp_at_trans.
+Print Assumptions universal_makes_comparable.
+
+(* === Self-reference === *)
+Print Assumptions self_reference_via_coarsening.
+Print Assumptions self_reference_impossible.
+
+(* === Dimension comparison === *)
+Print Assumptions dims_comparable_at.
+Print Assumptions dims_have_op_at.
